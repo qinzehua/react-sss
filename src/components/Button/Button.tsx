2ea5b753dde2cbe2c3bc9f21e254313bc4c5b275
Qin.zehua <77593508@qq..com>
2024-01-16 21:09:45 +0800

    btn first part base structor

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import classNames from "classnames";
+
+export enum ButtonSize {
+  Large = "lg",
+  Small = "sm",
+}
+
+export enum ButtonType {
+  Primary = "primary",
+  Default = "default",
+  Danger = "danger",
+  Link = "link",
+}
+
+type BaseButtonProps = {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+};
+
+export function Button(props: BaseButtonProps) {
+  const {
+    className,
+    disabled = false,
+    size,
+    btnType = ButtonType.Default,
+    children,
+    href,
+  } = props;
+
+  const classnames = classNames(
+    "btn",
+    {
+      [`btn-${btnType}`]: btnType,
+      [`btn-${size}`]: size,
+      disabled: btnType === ButtonType.Link && disabled,
+    },
+    className
+  );
+
+  if (btnType === ButtonType.Link && href) {
+    return (
+      <a href={href} className={classnames}>
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <button className={classnames} disabled={disabled}>
+        {children}
+      </button>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
-import App from "./App";
+import { Button, ButtonSize, ButtonType } from "./components/Button/Button";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Button disabled>Hello</Button>
+    <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+      Hello
+    </Button>
+    <Button btnType={ButtonType.Link} href="www.baidu.com" disabled>
+      Baidu
+    </Button>
   </React.StrictMode>
 );
