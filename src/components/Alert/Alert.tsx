363504fc96439f1d1b5496b9b7554cd1a0273066
Qin.zehua <77593508@qq..com>
2024-01-17 21:08:54 +0800

    feat: alert

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,3 +1,32 @@
-const Alert = () => {};
+import classNames from "classnames";
+
+export enum AlertType {
+  Success = "success",
+  Danger = "danger",
+  Warning = "warning",
+  Info = "info",
+}
+
+type AlertProps = {
+  message: string;
+  description?: string;
+  type: AlertType;
+};
+
+const Alert = (props: AlertProps) => {
+  const { message, description, type } = props;
+  const alertClass = classNames("alert", {
+    [`alert-${type}`]: type,
+  });
+
+  return (
+    <div className={alertClass}>
+      <div className="alert-content">
+        <div className="alert-message">{message}</div>
+        {description && <div className="alert-description">{description}</div>}
+      </div>
+    </div>
+  );
+};
 
 export { Alert };
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import { Button, ButtonSize, ButtonType } from "./components/Button/Button";
+import { Alert, AlertType } from "./components/Alert/Alert";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div>
+    <div
+      style={{
+        padding: 25,
+      }}
+    >
+      <h1>Button</h1>
+      <hr />
       <Button>Hello</Button>
       <Button disabled>Disabled Button</Button>
       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
@@ -23,6 +30,17 @@ root.render(
       <Button btnType={ButtonType.Link} href="www" disabled>
         Disabled Link
       </Button>
+
+      <h1>Alert</h1>
+      <hr />
+      <Alert
+        message="Success Text"
+        description="Detailed description and advice about successful copywriting."
+        type={AlertType.Success}
+      />
+      <Alert message="Info Text" type={AlertType.Info} />
+      <Alert message="Warning  Text" type={AlertType.Warning} />
+      <Alert message="Danger  Text" type={AlertType.Danger} />
     </div>
   </React.StrictMode>
 );
